d3f8c8ab5cc61083ccbd5a648e4f9617ea220a7a
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-07-06 00:05:12 +0100

    Configured vercel for hosting

--- hngstageone/settings.py
+++ hngstageone/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# ----> For vercel
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!gmua0lkuo7#loder62g)hmkv-#^%m&l68vtv74yj24tc_+35w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ------> For vercel
+DEBUG = False
+
+#ALLOWED_HOSTS = []
+
+# -----> For vercel
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# -----> For vercel
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
